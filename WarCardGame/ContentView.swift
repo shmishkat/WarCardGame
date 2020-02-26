ac6edd0353a0ec4f96921bfdcb33790e6d55b928
Sarowar H. Mishkat <mishu.sarowar@gmail.com>
2020-02-26 18:52:48 +0600

    final

--- WarCardGame/ContentView.swift
+++ WarCardGame/ContentView.swift
@@ -9,8 +9,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var randNum1 = 2
+    @State private var randNum2 = 2
+    
+    @State private var score1 = 0
+    @State private var score2 = 0
     var body: some View {
-        Text("Hello, World!")
+        
+        ZStack{
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+                        VStack {
+                            Spacer()
+                            Image("logo")
+                            Spacer()
+                            
+                        HStack{
+                                
+                                Image("card"+String(randNum1))
+                                Image("card"+String(randNum2))
+                            }
+                            
+                            Spacer()
+                            Button(action: {
+                                //randomize the state properties
+                                
+                                self.randNum1 = Int.random(in: 2...14)
+                                self.randNum2 = Int.random(in: 2...14)
+                                
+                                //update score
+                                
+                                if self.randNum1 > self.randNum2{
+                                    self.score1 += 1
+                                }else if self.randNum1 < self.randNum2 {
+                                    self.score2 += 1
+                                }
+                                
+                                
+                                
+                            }) {
+                                //Test("Button")
+                                Image("dealbutton")
+                                    .renderingMode(.original)
+                            }
+                            Spacer()
+                            
+                            HStack{
+                                VStack{
+                                    Text("Player")
+                                        .bold()
+                                        .padding(.bottom, 20)
+                                    Text(String(score1))
+                                }
+                                .padding(.leading, 20)
+                                .foregroundColor(.white)
+                                
+                                Spacer()
+                                    VStack{
+                                        Text("CPU")
+                                        .bold()
+                                        .padding(.bottom, 20)
+                                        Text(String(score2))
+                                }
+                                .padding(.trailing, 20)
+                                .foregroundColor(.white)
+                            }
+                            Spacer()
+                            
+                            
+                    }
+              }
+        
     }
 }
 
